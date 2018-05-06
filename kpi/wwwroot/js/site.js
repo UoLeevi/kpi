cb2811893f6478107b6f811d75ec6fcb61529311
Leevi Uotinen <uotinen.leevi@hotmail.com>
2018-05-06 12:41:13 +0300

    Add Chart.js

--- kpi/wwwroot/js/site.js
+++ kpi/wwwroot/js/site.js
@@ -1,1 +1,32 @@
-﻿
+﻿(function () {
+    var chartSettings = {
+        type: 'line',
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [{
+                label: "KPI",
+                backgroundColor: '#3366ff',
+                borderColor: '#f18805',
+                data: [0, 25, 10, 5, 35, 50, 100],
+            }]
+        },
+        options: {
+        }
+    };
+
+    document.addEventListener(
+        "DOMContentLoaded",
+        function (event) {
+            var yourcompanyKpiAppAnchor = document.getElementById("kpi-yourcompany");
+            var ctx = document.getElementById('kpi-chart').getContext('2d');
+
+            yourcompanyKpiAppAnchor.addEventListener(
+                "click",
+                drawChartOnce)
+
+            function drawChartOnce() {
+                var chart = new Chart(ctx, chartSettings);
+                yourcompanyKpiAppAnchor.removeEventListener("click", drawChartOnce);
+            }
+        });
+})();
