a6af645e81ca3ad8236893720dd6c507e2deeb8f
Leevi Uotinen <uotinen.leevi@hotmail.com>
2018-05-10 14:53:09 +0300

    Change chart dataset background color to use rgba

--- kpi/wwwroot/js/site.js
+++ kpi/wwwroot/js/site.js
@@ -31,7 +31,7 @@
         switch (chartType) {
             case "line":
             case "bar":
-                chartSettings.data.datasets[0].backgroundColor = '#3366ff';
+                chartSettings.data.datasets[0].backgroundColor = 'rgba(51, 102, 204, 0.4)';
                 chartSettings.options = {
                     legend: {
                         display: false
@@ -49,8 +49,9 @@
 
 
             case "radar":
+                chartSettings.data.datasets[0].backgroundColor = 'rgba(51, 102, 204, 0.1)';
                 chartSettings.data.datasets[0].pointBorderColor = '#f18805';
-                chartSettings.data.datasets[0].pointBackgroundColor = '#3366ff';
+                chartSettings.data.datasets[0].pointBackgroundColor = 'rgba(51, 102, 204, 0.4)';
                 chartSettings.data.datasets[0].lineTension = 0.1;
                 chartSettings.options = {
                     legend: {
@@ -66,18 +67,18 @@
 
             default:
                 chartSettings.data.datasets[0].backgroundColor = [
-                    changeHue('#3366ff', -30),
-                    changeHue('#3366ff', -25),
-                    changeHue('#3366ff', -20),
-                    changeHue('#3366ff', -15),
-                    changeHue('#3366ff', -10),
-                    changeHue('#3366ff', -5),
-                    changeHue('#3366ff', 0),
-                    changeHue('#3366ff', 5),
-                    changeHue('#3366ff', 10),
-                    changeHue('#3366ff', 15),
-                    changeHue('#3366ff', 20),
-                    changeHue('#3366ff', 25)
+                    'rgba(51, 102, 204, 0.00)',
+                    'rgba(51, 102, 204, 0.08)',
+                    'rgba(51, 102, 204, 0.16)',
+                    'rgba(51, 102, 204, 0.24)',
+                    'rgba(51, 102, 204, 0.32)',
+                    'rgba(51, 102, 204, 0.40)',
+                    'rgba(51, 102, 204, 0.48)',
+                    'rgba(51, 102, 204, 0.56)',
+                    'rgba(51, 102, 204, 0.64)',
+                    'rgba(51, 102, 204, 0.72)',
+                    'rgba(51, 102, 204, 0.80)',
+                    'rgba(51, 102, 204, 0.88)'
                 ];
                 chartSettings.options = {
                     legend: {
@@ -195,130 +196,6 @@
                 element.classList.add("hidden");
             });
         element.classList.add("fade-out");
-    }
-
-    // Changes the RGB/HEX temporarily to a HSL-Value, modifies that value 
-    // and changes it back to RGB/HEX.
-    // https://stackoverflow.com/a/17433060
-    function changeHue(
-        rgb,
-        degree) {
-        var hsl = rgbToHSL(rgb);
-        hsl.h += degree;
-        if (hsl.h > 360) {
-            hsl.h -= 360;
-        }
-        else if (hsl.h < 0) {
-            hsl.h += 360;
-        }
-        return hslToRGB(hsl);
-
-        // exepcts a string and returns an object
-        function rgbToHSL(rgb) {
-            // strip the leading # if it's there
-            rgb = rgb.replace(/^\s*#|\s*$/g, '');
-
-            // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
-            if (rgb.length == 3) {
-                rgb = rgb.replace(/(.)/g, '$1$1');
-            }
-
-            var r = parseInt(rgb.substr(0, 2), 16) / 255,
-                g = parseInt(rgb.substr(2, 2), 16) / 255,
-                b = parseInt(rgb.substr(4, 2), 16) / 255,
-                cMax = Math.max(r, g, b),
-                cMin = Math.min(r, g, b),
-                delta = cMax - cMin,
-                l = (cMax + cMin) / 2,
-                h = 0,
-                s = 0;
-
-            if (delta === 0) {
-                h = 0;
-            }
-            else if (cMax === r) {
-                h = 60 * (((g - b) / delta) % 6);
-            }
-            else if (cMax === g) {
-                h = 60 * (((b - r) / delta) + 2);
-            }
-            else {
-                h = 60 * (((r - g) / delta) + 4);
-            }
-
-            if (delta === 0) {
-                s = 0;
-            }
-            else {
-                s = (delta / (1 - Math.abs(2 * l - 1)));
-            }
-
-            return {
-                h: h,
-                s: s,
-                l: l
-            };
-        }
-
-        // expects an object and returns a string
-        function hslToRGB(hsl) {
-            var h = hsl.h,
-                s = hsl.s,
-                l = hsl.l,
-                c = (1 - Math.abs(2 * l - 1)) * s,
-                x = c * (1 - Math.abs((h / 60) % 2 - 1)),
-                m = l - c / 2,
-                r, g, b;
-
-            if (h < 60) {
-                r = c;
-                g = x;
-                b = 0;
-            }
-            else if (h < 120) {
-                r = x;
-                g = c;
-                b = 0;
-            }
-            else if (h < 180) {
-                r = 0;
-                g = c;
-                b = x;
-            }
-            else if (h < 240) {
-                r = 0;
-                g = x;
-                b = c;
-            }
-            else if (h < 300) {
-                r = x;
-                g = 0;
-                b = c;
-            }
-            else {
-                r = c;
-                g = 0;
-                b = x;
-            }
-
-            r = normalize_rgb_value(r, m);
-            g = normalize_rgb_value(g, m);
-            b = normalize_rgb_value(b, m);
-
-            return rgbToHex(r, g, b);
-        }
-
-        function normalize_rgb_value(color, m) {
-            color = Math.floor((color + m) * 255);
-            if (color < 0) {
-                color = 0;
-            }
-            return color;
-        }
-
-        function rgbToHex(r, g, b) {
-            return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-        }
     }
 
 })();
